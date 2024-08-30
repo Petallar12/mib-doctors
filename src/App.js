9308f7532ed25bfe29539f5bf462222e9043f65b
PH-Strawhat <139205442+PH-Strawhat@users.noreply.github.com>
2024-08-30 15:31:29 +0800

    try sa routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Doctor from './components/Doctor';
 import DoctorDetail from './components/DoctorDetail';
 
--- src/components/Doctor.js
+++ src/components/Doctor.js
@@ -198,8 +198,8 @@ const Doctor = () => {
 />
                         </div>
                         <h3>
-                            <a href={`/mib-doctors/doctor/${doctor.id}`}>{doctor.name.toUpperCase()}</a>
-                        </h3>
+    <Link to={`/doctor/${doctor.id}`}>{doctor.name.toUpperCase()}</Link>
+</h3>
                         <p><FaUserMd /> {doctor.speciality}</p>
                         <p><FaClinicMedical /> {doctor.clinic_name}</p>
                     </div>
